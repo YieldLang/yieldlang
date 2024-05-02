f9b92fb40547e615ac8330bf9b3ba975cda2091e
HK-SHAO <hk-shao@foxmail.com>
2024-05-02 21:48:07 +0800

    Bump version to v0.1.0 for release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,7 +88,7 @@ typehints_defaults = "comma"
 #
 html_theme = "furo"
 
-html_title = f"YieldLang <br/>v{VERSION}"
+html_title = f"YieldLang v{VERSION}"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
